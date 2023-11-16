9f857695e3a263c67e16bd00c6622d661af00923
Mark-James McDougall <contact@markjames.dev>
2023-11-16 11:19:52 -0500

    Add db migrations

---
+++ src/Migrations/Page/20231116045627_Pages.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sprocket.Migrations.Page
+{
+    /// <inheritdoc />
+    public partial class Pages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Collections",
+                table: "Collections");
+
+            migrationBuilder.RenameTable(
+                name: "Collections",
+                newName: "Pages");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pages",
+                table: "Pages",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pages",
+                table: "Pages");
+
+            migrationBuilder.RenameTable(
+                name: "Pages",
+                newName: "Collections");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Collections",
+                table: "Collections",
+                column: "Id");
+        }
+    }
+}
